72fc9c9a368e4d2f178669d7650b4c112d728cce
nldq092203 <lnguye220903@gmail.com>
2024-12-08 20:44:25 +0100

    production

--- backend/url_shortener/settings.py
+++ backend/url_shortener/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,10 +78,7 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default="postgresql://postgres:ogICIZnGMVfiEOLaNlOfxXjvOBLcbWEL@autorack.proxy.rlwy.net:56146/railway")
 }
 
 
